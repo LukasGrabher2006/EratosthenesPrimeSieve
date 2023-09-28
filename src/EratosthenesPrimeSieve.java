59af8ec7d4d8126c6c24a387677189c56143e6fc
Lukas <lukasgrabher414@gmail.com>
2023-09-28 11:44:26 +0200

    isPrime initialized

--- src/EratosthenesPrimeSieve.java
+++ src/EratosthenesPrimeSieve.java
@@ -1,10 +1,9 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ListResourceBundle;
 
-public class Main implements PrimeSieve{
+public class EratosthenesPrimeSieve implements PrimeSieve{
 
-    public static Main main = new Main();
+    public static EratosthenesPrimeSieve main = new EratosthenesPrimeSieve();
 
     public static void main(String[] args) {
 
@@ -20,22 +19,7 @@ public class Main implements PrimeSieve{
     @Override
     public boolean isPrime(int p) {
 
-        if (p <= 1) {
-            return false;
-        }
-        if (p <= 3) {
-            return true;
-        }
-        if (p % 2 == 0 || p % 3 == 0) {
-            return false;
-        }
-        for (int i = 5; i * i <= p; i += 6) {
-            if (p % i == 0 || p % (i + 2) == 0) {
-                return false;
-            }
-        }
 
-        return true;
     }
 
     @Override
